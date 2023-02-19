e5ead06d1705e571edae27b2fc515904e7cd5c67
ttefu021 <ttefu021@student.wethinkcode.co.za>
2023-02-20 00:58:46 +0200

    Improved the home page section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HomePageComponent } from './home_page/home-page.component';
 
 
 const routes: Routes = [
-  
+  { path: '', component: HomePageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home_page/home-page.component';
+import { LoginService } from './home_page/spotify-login/login.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
